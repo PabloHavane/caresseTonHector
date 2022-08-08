175fc29d6ce321f81f09660e043d4d088f16663d
Pablo Havane <oscar282004@gmail.com>
2022-08-08 15:25:30 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,57 @@
+// Created by Pablo Havane on 07/19/2022
+
+import React, { useState } from "react";
+import ReactPlayer from "react-player";
+import "./App.css"
+
+function Gif() {
+    return <div className="img-container">
+        <img src="/gif_caresse_ton_hector.gif" alt="GIF caresse ton Hector"></img>
+        <img src="/gros_boulard.gif" alt="GIF du gros boulard d'Hector" />
+    </div>
+        
+}
+
+function Music() {
+    return <ReactPlayer 
+        url="https://www.youtube.com/watch?v=7RUqluZyqYw"
+        width="0px"
+        height="0px"
+        playing={true}
+        controls={false}
+    />
+}
+
+// La logique pour les boutons est la même qui se répète : possibilté d'amélioration ? 
+function Button() {
+    const [show, setShow] = useState(false)
+    const [music, setMusic] = useState(true)
+    let textButton = "Appuyer ici pour caresser le Hector"
+    let speakerImage = <img src="/speaker.png" alt="icon de speaker"></img>
+
+    if (show === true) {
+        textButton = "Appuyer ici pour arrêter de caresser ce boulard de dieu"
+    }
+
+    if (music === false) {
+        speakerImage = <img src="/no-speaker.png" alt="icone de no speaker" />
+    }
+
+    return <div>
+        <div className="button-container">
+            <button id="main-button" onClick={() => setShow(!show)} >{textButton}</button>
+            <button onClick={() => setMusic(!music)}> {speakerImage} </button>
+        </div>
+
+        {show ? <Gif /> : null}
+        {music ? <Music /> : null}
+    </div>
+}
+
+function App() {
+    return <div className="App">
+        <Button />
+    </div>
+}
+
+export default App;
